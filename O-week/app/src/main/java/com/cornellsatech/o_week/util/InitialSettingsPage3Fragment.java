a106d1b1f0bbf88f4c06c22b07c5bb307db93df2
wsjnohyeah <ethan.hqc@icloud.com>
2017-11-03 22:38:27 -0400

    added initial settings page and their pageradapter, page fragments

---
+++ O-week/app/src/main/java/com/cornellsatech/o_week/util/InitialSettingsPage3Fragment.java
@@ -0,0 +1,20 @@
+package com.cornellsatech.o_week.util;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ethan on 10/29/17.
+ */
+
+public class InitialSettingsPage3Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.initial_settings_welcome, container,false);
+        return rootView;
+    }
+}
